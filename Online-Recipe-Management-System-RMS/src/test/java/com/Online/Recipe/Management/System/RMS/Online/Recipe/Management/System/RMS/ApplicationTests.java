72c3fac11fa8d809ec990bd9a0b4421311be5347
Malleshcr7 <telugumallesh79@gmail.com>
2025-05-24 13:45:31 +0530

    Initial commit

---
+++ Online-Recipe-Management-System-RMS/src/test/java/com/Online/Recipe/Management/System/RMS/Online/Recipe/Management/System/RMS/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Online.Recipe.Management.System.RMS.Online.Recipe.Management.System.RMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
